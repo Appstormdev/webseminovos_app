470244eeef71ce4cc75f3cddef7b82b03385d848
Ricardo Granvilla <rgranvilla@gmail.com>
2023-02-02 13:12:34 -0300

    feat:check description length and displays button view more if needed

--- src/components/PromoCardDetailDescription.tsx
+++ src/components/PromoCardDetailDescription.tsx
@@ -1,34 +1,47 @@
 import { Box, Button, Text } from "native-base";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 interface PromoCardDetailDescriptionProps {
   description: string;
 }
 
+const NUM_OF_LINES = 2;
+
 export function PromoCardDetailDescription({
   description,
 }: PromoCardDetailDescriptionProps) {
-  const [showAllDescription, setShowAllDescription] = useState<boolean>(false);
+  const [showMore, setShowMore] = useState<boolean>(false);
+  const [showExpandableButton, setShowExpandableButton] =
+    useState<boolean>(false);
+  const onTextLayout = useCallback((e: any) => {
+    setShowExpandableButton(e.nativeEvent.lines.length > NUM_OF_LINES);
+  }, []);
 
   return (
     <>
-      <Text numberOfLines={showAllDescription ? undefined : 2} mt={4}>
+      <Text
+        numberOfLines={showMore ? undefined : NUM_OF_LINES}
+        mt={4}
+        onTextLayout={onTextLayout}
+      >
         {description}
       </Text>
-      <Box w="full" alignItems="flex-end" mt={1}>
-        <Button
-          variant="ghost"
-          bg="gray.200"
-          w="90px"
-          p={0}
-          rounded="3xl"
-          onPress={() => setShowAllDescription(!showAllDescription)}
-        >
-          <Text color="gray.600">
-            {showAllDescription ? "reduzir..." : "expandir..."}
-          </Text>
-        </Button>
-      </Box>
+      {showExpandableButton && (
+        <Box w="full" alignItems="flex-end" mt={1}>
+          <Button
+            variant="ghost"
+            bg="gray.200"
+            w="90px"
+            p={0}
+            rounded="3xl"
+            onPress={() => setShowMore(!showMore)}
+          >
+            <Text color="gray.600">
+              {showMore ? "reduzir..." : "expandir..."}
+            </Text>
+          </Button>
+        </Box>
+      )}
     </>
   );
 }
