cab457d411edf5f392693a286fe494e9817f1ed1
Ricardo Granvilla <rgranvilla@gmail.com>
2023-02-02 12:59:29 -0300

    feat:add selectedOffer, offer, selectOffer and loadOffer

--- src/context/OffersContext.tsx
+++ src/context/OffersContext.tsx
@@ -3,14 +3,37 @@ import { createContext, ReactNode, useEffect, useState } from "react";
 import { OffersDTO } from "@dtos/OffersDTO";
 import { api } from "@services/api";
 import { getOffers, storeOffers } from "@storage/storageOffers";
+import { getFullAddress } from "@utils/addressTools";
 
 export type OffersContextDataProps = {
   offers: OffersDTO[];
+  selectedOffer: OffersDTO | null;
+  offer: OfferType | null;
   fetchSearchOffers: (term: string) => Promise<void>;
+  selectOffer: (offerId: string) => void;
+  loadOffer: () => void;
 };
 
 type OffersContextProviderProps = {
   children: ReactNode;
+};
+
+type OfferType = {
+  businessName: string;
+  businessLogo: string;
+  businessFullAddress: string;
+  businessWhatsappNumber: string;
+  carId: string;
+  carTitle: string;
+  carImage: string;
+  carBrand: string;
+  carModel: string;
+  carDescription: string;
+  carPrice: string;
+  carYear: string;
+  carEngine: string;
+  carFuelType: string;
+  carMileage: string;
 };
 
 export const OffersContext = createContext<OffersContextDataProps>(
@@ -21,6 +44,8 @@ export function OffersContextProvider({
   children,
 }: OffersContextProviderProps) {
   const [offers, setOffers] = useState<OffersDTO[]>([]);
+  const [offer, setOffer] = useState<OfferType | null>(null);
+  const [selectedOffer, setSelectedOffer] = useState<OffersDTO | null>(null);
 
   async function fetchSearchOffers(term: string) {
     try {
@@ -47,11 +72,48 @@ export function OffersContextProvider({
     loadOffers();
   }, []);
 
+  function selectOffer(offerId: string) {
+    const offer = offers.find((offer) => offer.id === offerId);
+
+    if (offer) setSelectedOffer(offer);
+    console.log("SELECTED:", offer);
+  }
+
+  function loadOffer() {
+    if (selectedOffer) {
+      const businessFullAddress = getFullAddress(selectedOffer);
+
+      const promo: OfferType = {
+        businessName: selectedOffer.name,
+        businessLogo: selectedOffer.avatar,
+        businessFullAddress,
+        businessWhatsappNumber: selectedOffer.phone,
+        carId: selectedOffer.id,
+        carTitle: selectedOffer.titulo_oferta,
+        carImage: selectedOffer.imagem_oferta,
+        carBrand: selectedOffer.marca_oferta,
+        carModel: selectedOffer.modelo_oferta,
+        carDescription: selectedOffer.descricao_oferta,
+        carPrice: selectedOffer.preco_oferta,
+        carYear: selectedOffer.ano_oferta,
+        carEngine: selectedOffer.motorizacao_oferta,
+        carFuelType: selectedOffer.combustivel_oferta,
+        carMileage: selectedOffer.km_oferta,
+      };
+
+      setOffer(promo);
+    }
+  }
+
   return (
     <OffersContext.Provider
       value={{
         offers,
+        selectedOffer,
+        offer,
+        loadOffer,
         fetchSearchOffers,
+        selectOffer,
       }}
     >
       {children}
