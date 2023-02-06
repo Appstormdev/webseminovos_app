323d734396fc83f72e1c8bfe0c3b983de043a345
Ricardo Granvilla <rgranvilla@gmail.com>
2023-02-06 12:26:17 -0300

    refactor:move App.tsx from rootDir to src folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import "expo-dev-client";
+import OneSignal from "react-native-onesignal";
 import {
-  Keyboard,
+  Alert,
   NativeEventEmitter,
   NativeModules,
-  Platform,
   StatusBar,
 } from "react-native";
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, useToast } from "native-base";
+
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import {
   useFonts,
@@ -14,28 +16,34 @@ import {
   Inter_900Black,
 } from "@expo-google-fonts/inter";
 
-import { THEME } from "./src/theme";
+import { THEME } from "@theme/index";
 
 import { Loading } from "@components/Loading";
 import { AppContextProviders } from "@context/index";
 import { Routes } from "@routes/index";
-//@ts-ignore
-import { initialize } from "react-native-zapt-sdk";
-import { useEffect } from "react";
-import { startListenterZaptSDK } from "@services/zapt";
-
-const ZAPT_PLACE_ID = "-nmf8aqficgvin6zka2_";
+import { useEffect, useState } from "react";
+import {
+  initialize,
+  requestPermissionsBackground,
+  requestPermissions,
+  addLocationListener,
+  calculateLocation,
+  //@ts-ignore
+} from "react-native-zapt-sdk";
+import useBLE from "@services/useBLE";
+const placeId = "-nmf8aqficgvin6zka2_";
+OneSignal.setAppId("74d0b39e-d9e3-4c97-8a4f-7cf8429b5b2b");
 
 export default function App() {
+  useEffect(() => {
+    requestPermissions((isGranted: boolean) => {});
+  }, []);
+
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_700Bold,
     Inter_900Black,
   });
-
-  useEffect(() => {
-    initialize(ZAPT_PLACE_ID).then(startListenterZaptSDK);
-  }, []);
 
   return (
     <NativeBaseProvider theme={THEME}>
